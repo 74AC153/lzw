3a09d4f8d7847b349a4ed3249b4cab0564edc2b2
Thomas Bastiani <thomas.bastiani@v-nova.com>
2015-08-13 15:11:02 +0100

    Pack bytes depending on dictionary size

--- lzw_pipe.c
+++ lzw_pipe.c
@@ -1,6 +1,9 @@
 #include <assert.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <limits.h>
+#include <alloca.h>
 
 #include "lzw.h"
 
@@ -18,6 +21,49 @@ static int emit_char(void *p, unsigned char ch)
 	return 0;
 }
 
+static unsigned bytesPerSymbol()
+{
+	assert(DICTSIZE <= (1llu << (CHAR_BIT * sizeof(unsigned))));
+	unsigned symBytes = 0;
+	unsigned dictSize = DICTSIZE - 1;
+	while (dictSize)
+	{
+		symBytes++;
+		dictSize = dictSize >> CHAR_BIT;
+	}
+	return symBytes;
+}
+
+static void pack(unsigned *input, size_t nmemb, uint8_t *output)
+{
+	unsigned symBytes = bytesPerSymbol();
+	for (size_t i = 0; i < nmemb; i++)
+	{
+		for (unsigned b = 0; b < symBytes; b++)
+		{
+			output[i * symBytes + b] = (uint8_t)
+				((input[i] >> (CHAR_BIT * b)) & 0xff);
+		}
+	}
+}
+
+static void unpack(uint8_t *input, size_t nmemb, unsigned *output)
+{
+	unsigned symBytes = bytesPerSymbol();
+	size_t symCount = nmemb / symBytes;
+
+	for (size_t i = 0; i < symCount; i++)
+	{
+		unsigned value = 0;
+		for (unsigned b = 0; b < symBytes; b++)
+		{
+			value += input[i * symBytes + b] << (b * CHAR_BIT);
+		}
+
+		output[i] = value;
+	}
+}
+
 static int encode(void)
 {
 	struct lzw_state enc;
@@ -25,6 +71,8 @@ static int encode(void)
 
 	char input[1000];
 	unsigned compressed[1024];
+	unsigned symBytes = bytesPerSymbol();
+	uint8_t *writeBuffer = alloca(1024 * symBytes);
 	size_t len;
 	unsigned *comp_curs;
 	unsigned complen;
@@ -36,14 +84,16 @@ static int encode(void)
 			assert(!lzw_encode(&enc, emit_code, (void *)&comp_curs, input[i]));
 		complen = comp_curs - compressed;
 
-		if (fwrite(compressed, sizeof(unsigned), complen, stdout) != complen)
+		pack(compressed, complen, writeBuffer);
+		if (fwrite(writeBuffer, symBytes, complen, stdout) != complen)
 			return -1;
 	}
 
 	comp_curs = compressed;
 	assert(!lzw_encode_finish(&enc, emit_code, (void *) &comp_curs));
 	complen = comp_curs - compressed;
-	if (fwrite(compressed, sizeof(unsigned), complen, stdout) != complen)
+	pack(compressed, complen, writeBuffer);
+	if (fwrite(writeBuffer, symBytes, complen, stdout) != complen)
 		return -1;
 
 	return 0;
@@ -54,11 +104,14 @@ static int decode(void)
 	struct lzw_state dec;
 	lzw_state_init(&dec);
 
+	unsigned symBytes = bytesPerSymbol();
+	uint8_t *readBuffer = alloca(50 * symBytes);
 	unsigned input[50];
 	unsigned char output[1024];
 	size_t len;
 
-	while ((len = fread(input, sizeof(unsigned), 50, stdin)) > 0) {
+	while ((len = fread(readBuffer, symBytes, 50, stdin)) > 0) {
+		unpack(readBuffer, len * symBytes, input);
 		unsigned char *dec_curs = &output[0];
 		for(unsigned i = 0; i < len; i++)
 			assert(!lzw_decode(&dec, emit_char, (void *)&dec_curs, input[i]));
